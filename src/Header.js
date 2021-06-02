894e9e0e633528a921f1f8f930a69502b16da4e6
Jehan98 <Jehan98@users.noreply.github.com>
2021-06-02 14:45:30 +0530

    Meme Generator Finished

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 function Header(){
     return (<header className="menu-area">
         <div className="header-div">
-            <img className="img-header" src="http://assets.stickpng.com/thumbs/580b585b2edbce24c47b2a27.png" />
+            <img className="img-header" src="http://assets.stickpng.com/thumbs/580b585b2edbce24c47b2a27.png" alt="memephoto" />
             <h1 className="h1-header">Meme Generator</h1>
         </div>
     </header>)
--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -3,11 +3,71 @@ import React from "react";
 class MemeGenerator extends React.Component{
     constructor(){
         super()
-        this.state = {}
+        this.state = {
+            topText:"",
+            bottomText:"",
+            randImg:"http://i.imgflip.com/1bij.jpg"
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    componentDidMount(){
+        fetch("https://api.imgflip.com/get_memes")
+        .then(response => response.json())
+        .then(response => this.setState({
+            allMemeImgs: response.data.memes
+        }))
+    }
+
+    handleChange(event){
+        const name = event.target.name
+        const value = event.target.value
+        this.setState({
+            [name]: value
+        })
+    }
+
+    handleSubmit(event){
+        event.preventDefault()
+        const randNum = Math.floor(Math.random()*this.state.allMemeImgs.length)
+        const randImg = this.state.allMemeImgs[randNum].url
+        this.setState({
+            randImg: randImg
+        })
     }
 
     render(){
-        return <h1>Mme</h1>
+        return (
+            <div className="form-div">
+                <form onSubmit={this.handleSubmit}>
+                        <input typr="text" 
+                            name="topText" 
+                            value={this.state.topText} 
+                            placeholder="Enter Top Text" 
+                            onChange={this.handleChange} 
+                        />
+                    <br/>
+                    <br/>
+                        <input typr="text" 
+                            name="bottomText" 
+                            value={this.state.bottomText} 
+                            placeholder="Enter Bottom Text" 
+                            onChange={this.handleChange} 
+                        />
+                    <br/>
+                    <br/>
+                    <input type="submit" name="submit" value="Gen"/>
+                </form>
+                <div className="meme-pic">
+                    <br/>
+                    <h2 className="text-center">{this.state.topText}</h2>
+                    <img src={this.state.randImg} className="img-memepic" alt="memepic" />
+                    <h2 className="text-center">{this.state.bottomText}</h2>
+                </div>
+            </div>
+        )
+        
     }
 } 
 
